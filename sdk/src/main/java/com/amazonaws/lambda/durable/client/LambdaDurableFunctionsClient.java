780b683b3f14182953aa043e491a1bc5c3212113
Philipp Page <pagejep@amazon.com>
2025-12-12 17:20:58 +0100

    Fix bug when loadingAllOperations (making unncessary API calls).

--- sdk/src/main/java/com/amazonaws/lambda/durable/client/LambdaDurableFunctionsClient.java
+++ sdk/src/main/java/com/amazonaws/lambda/durable/client/LambdaDurableFunctionsClient.java
@@ -10,7 +10,7 @@ public class LambdaDurableFunctionsClient implements DurableExecutionClient {
     
     public LambdaDurableFunctionsClient(LambdaClient AWSLambdaSDKClient) {
         if (AWSLambdaSDKClient == null) {
-            this.AWSLambdaSDKClient =  LambdaClient.create();;
+            this.AWSLambdaSDKClient =  LambdaClient.create();
         } else {
             this.AWSLambdaSDKClient = AWSLambdaSDKClient;
         }
